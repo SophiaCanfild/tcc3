ae74642e7e867f4ccc461370ec83e7f5fe7abe65
sophia <sophia.saladini@escola.pr.gov.br>
2026-06-22 09:30:05 -0300

    supbase

--- supabase.js
+++ supabase.js
@@ -1,1 +1,10 @@
+const SUPABASE_URL ="https://ofqyqorsedxveersqful.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9mcXlxb3JzZWR4dmVlcnNxZnVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA4NjYwNTUsImV4cCI6MjA5NjQ0MjA1NX0.ZKz40dXHnloi0w8n87MvExq1RQZe_nyThfxqvGIj-lc";
 
+const supabaseclient =
+window.supabase.createClient(
+    SUPABASE_KEY,
+    SUPABASE_URL
+);
+
+window.supabaseclient = supabaseclient;
